3f0117d900506c43518d4a682473b750804ef018
jpranays <pranay1315@gmail.com>
2024-07-21 15:50:02 +0530

    Initial commit

---
+++ rollup.config.mjs
@@ -0,0 +1,38 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import terser from "@rollup/plugin-terser";
+import dts from "rollup-plugin-dts";
+
+export default [
+	{
+		input: "src/index.ts",
+		output: [
+			{
+				file: "dist/index.cjs.js",
+				format: "cjs",
+				sourcemap: false,
+			},
+			{
+				file: "dist/index.esm.js",
+				format: "esm",
+				sourcemap: false,
+			},
+		],
+		external: ["react"],
+		plugins: [
+			resolve(),
+			commonjs(),
+			typescript({
+				tsconfig: "./tsconfig.json",
+				outputToFilesystem: true,
+			}),
+			terser(),
+		],
+	},
+	{
+		input: "./dist/types/index.d.ts",
+		output: [{ file: "dist/index.d.ts", format: "es" }],
+		plugins: [dts()],
+	},
+];
